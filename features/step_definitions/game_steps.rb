60e4a8abd3ded80bdbf75fd00f448d671a254185
Ole Christian Rynning <oc@rynning.no>
2010-01-08 10:49:37 +0100

    Removed crap

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,14 +1,3 @@
 # encoding: utf-8
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
 
-Given /^at (.*) har (.*)$/ do |player_name, hand|
-#  player = Game.player player_name
-#  player.hand = hand
-end
-Given /^(.*) har (.*)$/ do |player_name, hand|
-#  player ||= Game.player player_name
-  pending
-end
-Then /^skal (.*) vinne$/ do |spiller|
-  pending
-end
